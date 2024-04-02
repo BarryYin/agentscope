ff919ed69931374a53888237d17957f8647e9356
BarryYin <1509008060@qq.com>
2024-04-02 17:55:39 +0800

    Update gomoku_agent.py

--- examples/game_gomoku/code/gomoku_agent.py
+++ examples/game_gomoku/code/gomoku_agent.py
@@ -8,23 +8,44 @@ import json
 from agentscope.message import Msg
 from agentscope.agents import AgentBase
 from agentscope.models import ModelResponse
+import re
 
 
 HINT_PROMPT = """
-You should respond in the following format, which can be loaded by json.loads in Python:
-{{
+You should respond in the json format, which can be loaded by json in Python(pleas don't show 'json'):
+{
     "thought": "analyze the present situation, and what move you should make",
     "move": [row index, column index]
-}}
-"""  # noqa
-
+}
+few show like this:
+{
+    "thought": "I should aim to create a line of my pieces while also blocking my opponent from forming a line. Currently, there is a potential threat from the 'o' piece at [5, 6] which could lead to a vertical line. To counter this threat and potentially create my own line, I should place my piece at [6, 6]. This move will block the opponent's vertical line and create a potential horizontal line for me.",
+    "move": [
+        7,
+        7
+    ]
+}
+Please ensure that your response follows the specified format and includes the necessary fields.
+# forbitten
+this format is forbitten, don't show 'json'
+```json\n{ "thought": "", "move": [ 7, 7 ] }```
+"""  
+# noqa
 
 def parse_func(response: ModelResponse) -> ModelResponse:
     """Parse the response from the model into a dict with "move" and "thought"
     keys."""
-    res_dict = json.loads(response.text)
-    if "move" in res_dict and "thought" in res_dict:
-        return ModelResponse(raw=res_dict)
+    print(response.text)
+    match = re.search(r'```json\n(.*?)```', response.text, re.DOTALL)
+    if match is not None:
+        json_text = match.group(1)  # group(1)返回第一个括号中匹配的部分
+        res_dict = json.loads(json_text)
+        if "move" in res_dict and "thought" in res_dict:
+            return ModelResponse(raw=res_dict)
+    elif(match is None):
+        res_dict = json.loads(response.text)
+        if "move" in res_dict and "thought" in res_dict:
+            return ModelResponse(raw=res_dict)
     else:
         raise ValueError(
             f"Invalid response format in parse_func "
